09ed2052641ebc05c4d4cfb6576f0dfedd763f61
tdhood <taylordannhood@gmail.com>
2022-12-01 14:15:24 -0800

    day 1 part 1 updated

--- day1/Day1Puzzle1.py
+++ day1/Day1Puzzle1.py
@@ -34,19 +34,14 @@ def total(array):
     return total
 
 def find_calories_per_elf(elf_packs):
-    elves = {i: total(calories) for i, calories in enumerate(elf_packs, 1)}
+    elves = [total(calories) for calories in elf_packs]
     return elves
-    
 
-elves_and_packs = find_calories_per_elf(elf_packs)
-print('elves and total calories+++++++', elves_and_packs)
+elves_packs = find_calories_per_elf(elf_packs)
+print('elves and total calories+++++++', elves_packs)
 
-def find_max(dict):
-    calories = list(dict.values())
-    elves = list(dict.keys())
-    max_calories = max(calories)
+def find_max(array):
+    max_calories = max(array)
     print('max_cal=====', max_calories)
-    elf_with_most_calories = elves[calories.index(max_calories)]
-    print('elf_with_most_calories', elf_with_most_calories)
 print('find elf with the most' ,find_max(elves_and_packs))
 
